c10c769a6379be5898a1355ceb1b6a7f8de5443a
DatSung <datsung.dev@gmail.com>
2024-05-01 17:44:57 +0700

    update

--- ADPSys.DataAccess/obj/Debug/net8.0/ADPSys.DataAccess.AssemblyInfo.cs
+++ ADPSys.DataAccess/obj/Debug/net8.0/ADPSys.DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADPSys.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22861cf5d782ca3b6c80e395e4c9d4baa1bf74b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898db4be85192307db7f92955aacc134e4f5fbde")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADPSys.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADPSys.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
